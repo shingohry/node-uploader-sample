3b1319ded9b961c374313fbf3bdad852385faf2d
Shingo Hiraya <shingohiraya@gmail.com>
2017-02-24 17:58:06 +0900

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', upload.single('file'), function(req, res) {
+  res.json({ 'result': 'success!' });
 });
 
 module.exports = router;
